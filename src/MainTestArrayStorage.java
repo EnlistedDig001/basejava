9e64465905187e64179fb144be3bea3394177169
EnlistedDig001 <vsokolov0011@gmail.com>
2018-11-25 23:40:08 +0300

    Default test replaced with self-realized

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,10 +2,13 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        /**
+         * Default ArrayStorage test.
+         */
+        /*Resume r1 = new Resume();
         r1.uuid = "uuid1";
         Resume r2 = new Resume();
         r2.uuid = "uuid2";
@@ -27,10 +30,50 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.clear();
         printAll();
 
+        System.out.println("Size: " + ARRAY_STORAGE.size()); */
+
+        Resume r1 = new Resume();
+        r1.uuid = "uuid1";
+        Resume r2 = new Resume();
+        r2.uuid = "uuid2";
+        Resume r3 = new Resume();
+        r3.uuid = "uuid3";
+        Resume r5 = new Resume();
+        r5.uuid = "uuid5";
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r5);
+
+
+        System.out.println("Get r3: " + ARRAY_STORAGE.get(r3.uuid));
+        System.out.println("Get r5: " + ARRAY_STORAGE.get(r5.uuid));
+
+
         System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        printAll();
+
+        ARRAY_STORAGE.delete(r3.uuid);
+        printAll();
+        ARRAY_STORAGE.update(r5, r2);
+
+        printAll();
+
+        ARRAY_STORAGE.clear();
+
+        printAll();
+
+        ARRAY_STORAGE.save(r2);
+
+        printAll();
+
+
+
+
     }
 
-    static void printAll() {
+    private static void printAll() {
         System.out.println("\nGet All");
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
