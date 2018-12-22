0fa970038e2beeb8d4b805a63ea01433080eebac
EnlistedDig001 <vsokolov0011@gmail.com>
2018-12-22 11:29:25 +0300

    Resume duplication while deleting fixed

--- src/ru/enlis/basejava/storage/SortedArrayStorage.java
+++ src/ru/enlis/basejava/storage/SortedArrayStorage.java
@@ -18,6 +18,9 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     protected void deleteSavingOrder(int index) {
         System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+        if (size == MAX_SIZE) {
+            storage[size - 1] = null;
+        }
     }
 
     protected int indexInStorage(String uuid) {
